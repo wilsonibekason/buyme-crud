d7a977467024352165d40ec4ab7a06706dd4df6f
wilsonibekason <wanbekgithub@gmail.com>
2022-04-14 09:03:37 -0700

    added cart section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { Cart, Navbar, Products } from './container';
+import {commerce } from './lib/commerce'
 
 function App() {
+  const [cart, setCart] = useState({});
+  const [products, setProducts] = useState([]) ;
+  const [order, setOrder] = useState({});
+  
+ const fetchProducts  =  async() => {
+  const {data} = await commerce.products.list();
+
+  setProducts(data);
+ };
+  
+ const fetchCart = async ()=> {
+   setCart(await commerce.cart.retrieve());
+ };
+
+ // add to cart functionality
+ const handleAddToCart = async(productId, quantity) => {
+   const item = await commerce.cart.add(productId, quantity);
+   setCart(item.cart);
+ };
+     
+  useEffect(() => {
+     fetchProducts();
+     fetchCart();
+    }, []);
+
+    console.log(cart);
+  
+
   return (
-    <div className="App">
-      <h1>helo</h1>
+    <div>
+     <Navbar totalItems={cart.total_items}/>
+     <Products products={products} onAddToCart={handleAddToCart}/>
+     <Cart cart={cart}/>
     </div>
   );
 }
---
+++ src/container/cart/cartItems/CartItems.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import useStyles from './style';
+
+const CartItems = () => {
+  return (
+    <div>CartItems</div>
+  )
+}
+
+export default CartItems
